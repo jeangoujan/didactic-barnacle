cbeb64d01e65db50143b4ce8b980ba8d7ab4ad2c
jeangoujan <104026427+jeangoujan@users.noreply.github.com>
2024-04-10 00:19:33 +0500

    skipping 2,3 tests

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -16,17 +16,17 @@ def test_two_type_of_delivery_is_available_on_Web(page: Page):
     expect(page.get_by_text("Получение через Пункт выдачи"), "Доставка ПВЗ не должна отображаться").not_to_be_visible()
 
 
-def test_new_order_in_city(page: Page):
-    home_page = HomePage(page)
-    home_page.fillSomethingAndSearch(home_page.search_item)
-
-    select_seller_btn = page.get_by_role("button", name="Выбрать", exact=True)
-    select_seller_btn.click()
-    time.sleep(5)
-
-
-
-def test_four_type_of_delivery_is_available_on_webView(setup_page):
-    home_page = HomePage(setup_page)
-    home_page.fillSomethingAndSearchMobile(home_page.search_item)
-    expect(setup_page.get_by_text("Получение через Пункт выдачи"), "Доставка ПВЗ не отображается").to_be_visible()
+# def test_new_order_in_city(page: Page):
+#     home_page = HomePage(page)
+#     home_page.fillSomethingAndSearch(home_page.search_item)
+#
+#     select_seller_btn = page.get_by_role("button", name="Выбрать", exact=True)
+#     select_seller_btn.click()
+#     time.sleep(5)
+#
+#
+#
+# def test_four_type_of_delivery_is_available_on_webView(setup_page):
+#     home_page = HomePage(setup_page)
+#     home_page.fillSomethingAndSearchMobile(home_page.search_item)
+#     expect(setup_page.get_by_text("Получение через Пункт выдачи"), "Доставка ПВЗ не отображается").to_be_visible()
